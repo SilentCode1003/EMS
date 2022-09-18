70642592882e415150d64eff6ebb10c16c5d9d5b
SilentCode1003 <j0s3ph0r3nc10@gmail.com>
2022-09-18 20:33:25 +0800

    Added m2cashequipment update
    Signed-off-by: SilentCode1003 <j0s3ph0r3nc10@gmail.com>

--- EMS/routes/m2cashequipment.js
+++ EMS/routes/m2cashequipment.js
@@ -9,6 +9,9 @@ const { create } = require('xmlbuilder2');
 var xml2js = require('xml2js');
 var parser = new xml2js.Parser();
 
+var targetDir = '/data/m2cash/equipment/';
+var targetPath = '/data/m2cash/equipment';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('m2cashequipment', { title: 'Equipment Monitoring System' });
@@ -18,7 +21,6 @@ module.exports = router;
 
 router.post('/save', function(req, res, next) {
   var status = 'ONHAND';
-  var targetDir = '/data/m2cash/equipment/';
   var storename = req.body.storename;
   var address = req.body.address;
   var area = req.body.area;
@@ -75,9 +77,6 @@ router.post('/save', function(req, res, next) {
 
 let data_arr = [];
 router.get('/LodaData', function(req, res, next) {
-  var targetDir = '/data/m2cash/equipment/';
-  var targetPath = '/data/m2cash/equipment';
-
   output(__dirname + targetPath);
 
   async function getData(fullpath){
@@ -148,4 +147,70 @@ router.get('/LodaData', function(req, res, next) {
   })
 
   data_arr = [];
+});
+
+router.post('/Update',function(req, res, next){
+  var filename = req.body.filename;
+  var datedeploy = req.body.datedeploy;
+  var personel = req.body.personel;
+  var status = req.body.status;
+
+  xmlFileToJs(filename, function (err, obj) {
+    if (err){
+      res.json({
+        msg: 'error',
+        data: err
+      });
+    }
+    var data = obj['M2CashDeviceInfo'];
+    console.log(data);
+
+    //Xml Details
+    const root = create({ version: '1.0', encoding: "UTF-8", standalone: "yes" })
+    .ele('M2CashDeviceInfo')
+      .ele('StoreName').txt(data['StoreName']).up()
+      .ele('Address').txt(data['Address']).up()
+      .ele('Area').txt(data['Area']).up()
+      .ele('DeviceNumber').txt(data['DeviceNumber']).up()
+      .ele('PortType').txt(data['PortType']).up()
+      .ele('DeviceAdaptor').txt(data['DeviceAdaptor']).up()
+      .ele('CableType').txt(data['CableType']).up()
+      .ele('Modem').txt(data['Modem']).up()
+      .ele('ModemSerial').txt(data['ModemSerial']).up()
+      .ele('SIMCard').txt(data['SIMCard']).up()
+      .ele('ModemAdaptor').txt(data['ModemAdaptor']).up()
+      .ele('PatchCable').txt(data['PatchCable']).up()
+      .ele('DateDeploy').txt(datedeploy).up()
+      .ele('Personel').txt(personel).up()
+      .ele('Status').txt(status).up()
+    .up();
+    const xml = root.end({ prettyPrint: true });
+      console.log(xml);
+
+      //Write and Save xml details into XML file
+      let fullFileName = __dirname + targetDir + filename;
+
+      fs.writeFileSync(fullFileName, xml, function(err) {
+        if (err) throw err;
+          res.json({
+            msg: 'error',
+            data: err
+          });
+        });
+
+        res.json({
+          msg:'success'
+        });
+  });
+
+  //#region xmlFileToJs
+    function xmlFileToJs(filename, cb) {
+      var filepath = path.normalize(path.join(__dirname + targetDir, filename));
+      fs.readFile(filepath, 'utf8', function (err, xmlStr) {
+          if (err) throw (err);
+          xml2js.parseString(xmlStr, {}, cb);
+      });
+    }
+  //#endregion 
+
 });
